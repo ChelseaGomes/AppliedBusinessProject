2ce5288c84a1e6bceeb5fc412c8510bb4f8c2681
Chelsea Gomes <s130759@ap.be>
2024-11-08 11:40:04 +0000

    Verplichte velden aanmaken

--- app/expense-creation/webapp/controller/Expense.controller.js
+++ app/expense-creation/webapp/controller/Expense.controller.js
@@ -1,12 +1,54 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
-function (Controller) {
+function (Controller, MessageBox) {
     "use strict";
 
     return Controller.extend("expensecreation.controller.Expense", {
+       
         onInit: function () {
+            // Maken van een JSON model waar we de data van onze prospect in kunnen bewaren
+            let oExpenseModel = new sap.ui.model.json.JSONModel({
 
+                
+        project_name      : "",
+        project_leader    : "",
+        start_date        : null,
+        category          : "",
+        financing_type    : "",
+        execution_months  : ""
+
+
+                
+            });
+
+           
+            this.getView().setModel(oExpenseModel, "expenseModel");
+        },
+        onProspectCreation: function(oEvent){
+            
+            var oModel = this.getOwnerComponent().getModel()
+           
+            var oListBinding = oModel.bindList("/Expense", undefined, undefined, undefined, { $$updateGroupId: "createExpense" });
+            
+            var oContext = oListBinding
+                .create(this.getView().getModel("expenseModel").getData());
+            // oData werkt aan de hand van batches om te communiceren met de server
+            this.getOwnerComponent().getModel().submitBatch("createExpense")
+                .then(function () {
+				// De expense is aangemaakt
+				MessageBox.alert("Changes have been saved", {
+					icon : sap.m.MessageBox.Icon.SUCCESS,
+					title : "Success"
+				});
+			}, function (oError) {
+                // Er ging iets fout tijdens het aanmaken van de expense
+				MessageBox.alert(oError.message, {
+					icon : sap.m.MessageBox.Icon.ERROR,
+					title : "Unexpected Error"
+				});
+			});
         }
     });
 });
