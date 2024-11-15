725b047859746673a4ea81364a44b16a8d9c85da
Chelsea Gomes <s130759@ap.be>
2024-11-15 11:31:25 +0000

    Deploy Settings

---
+++ app/expense-creation/dist/controller/App.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(e){"use strict";return e.extend("expensecreation.controller.App",{onInit:function(){}})});
+//# sourceMappingURL=App.controller.js.map
