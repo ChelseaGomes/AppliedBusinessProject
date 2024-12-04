9ca72b4c2857997a387ada59fd5fec66a4e92fbd
Chelsea Gomes <s130759@ap.be>
2024-12-04 23:01:13 +0000

    Expense Creation App details

--- app/expense-creation/webapp/Component.js
+++ app/expense-creation/webapp/Component.js
@@ -1,35 +1,51 @@
 /**
  * eslint-disable @sap/ui5-jsdocs/no-jsdoc
  */
-
 sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "expensecreation/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "expensecreation/model/models"
+],
+function (UIComponent, Device, models) {
+    "use strict";
 
-        return UIComponent.extend("expensecreation.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend("expensecreation.Component", {
+        metadata: {
+            manifest: "json"
+        },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+        /**
+         * De component wordt geïnitialiseerd door UI5 tijdens het opstarten van de app
+         * en roept de init-methode één keer aan.
+         * @public
+         * @override
+         */
+        init: function () {
+            // Roep de init-methode van de basiscomponent aan
+            UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
+            // Routing activeren
+            this.getRouter().initialize();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+            // Stel het apparaatmodel in
+            this.setModel(models.createDeviceModel(), "device");
+
+            // Laad aangepast CSS-bestand
+            this._loadCustomCSS();
+        },
+
+        /**
+         * Laadt een aangepast CSS-bestand in de applicatie.
+         * @private
+         */
+        _loadCustomCSS: function () {
+            sap.ui.getCore().loadLibrary("sap.ui.core", {
+                async: true
+            }).then(function () {
+                // Voeg hier je aangepaste CSS-bestand toe
+                jQuery.sap.includeStyleSheet(sap.ui.require.toUrl("app/expense-creation/webapp/css/style.css"));
+            });
+        }
+    });
+}
 );
--- app/expense-creation/webapp/controller/Expense.controller.js
+++ app/expense-creation/webapp/controller/Expense.controller.js
@@ -1,62 +1,112 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox"
-],
-function (Controller, MessageBox) {
+], function (Controller, MessageBox) {
     "use strict";
 
     return Controller.extend("expensecreation.controller.Expense", {
-       
+        /**
+         * Functie voor initiële setup.
+         */
         onInit: function () {
-            // Maken van een JSON model waar we de data van onze expense in kunnen bewaren
+            // Maken van een JSON-model voor de expense data
             let oExpenseModel = new sap.ui.model.json.JSONModel({
-
-                
-        project_name      : "",
-        project_leader    : "",
-        start_date        : null,
-        category          : "",
-        financing_type    : "",
-        execution_months  : "",
-        current_co2_impact:"",
-        expected_co2_impact:"",
-        current_water_consumption:"",
-        expected_water_consumption:"",
-        green_payback:"",
-        green_energy_output:"", 
-        description:""
-
-        
-                
+                project_name: "",
+                project_leader: "",
+                start_date: null,
+                category: "",
+                financing_type: "",
+                execution_months: "",
+                current_co2_impact: "",
+                expected_co2_impact: "",
+                current_water_consumption: "",
+                expected_water_consumption: "",
+                green_payback: "",
+                green_energy_output: "",
+                description: ""
             });
 
-           
+            // Zet het model in de huidige View
             this.getView().setModel(oExpenseModel, "expenseModel");
-            
         },
-        onExpenseCreation: function(oEvent){
-            
-            var oModel = this.getOwnerComponent().getModel()
-           
-            var oListBinding = oModel.bindList("/Expenses", undefined, undefined, undefined, { $$updateGroupId: "createExpense" });
-            
-            var oContext = oListBinding
-                .create(this.getView().getModel("expenseModel").getData());
-            // oData werkt aan de hand van batches om te communiceren met de server
-            this.getOwnerComponent().getModel().submitBatch("createExpense")
+
+        /**
+         * Functie om naar het volgende tabblad te navigeren.
+         */
+        onNextTab: function () {
+            let oIconTabBar = this.getView().byId("_IDGenIconTabBar"); // Haal IconTabBar op
+            let sSelectedKey = oIconTabBar.getSelectedKey(); // Bepaal huidige tab
+            let aItems = oIconTabBar.getItems(); // Alle tabs
+
+            // Zoek het volgende tabblad
+            for (let i = 0; i < aItems.length; i++) {
+                if (aItems[i].getKey() === sSelectedKey && i + 1 < aItems.length) {
+                    oIconTabBar.setSelectedKey(aItems[i + 1].getKey()); // Navigeer naar het volgende tabblad
+                    break;
+                }
+            }
+        },
+
+        /**
+         * Functie om naar het vorige tabblad te navigeren.
+         */
+        onPreviousTab: function () {
+            let oIconTabBar = this.getView().byId("_IDGenIconTabBar"); // Haal IconTabBar op
+            let sSelectedKey = oIconTabBar.getSelectedKey(); // Bepaal huidige tab
+            let aItems = oIconTabBar.getItems(); // Alle tabs
+
+            // Zoek het vorige tabblad
+            for (let i = 0; i < aItems.length; i++) {
+                if (aItems[i].getKey() === sSelectedKey && i - 1 >= 0) {
+                    oIconTabBar.setSelectedKey(aItems[i - 1].getKey()); // Navigeer naar het vorige tabblad
+                    break;
+                }
+            }
+        },
+
+        /**
+         * Functie om de gegevens te voltooien en te verzenden.
+         */
+        onComplete: function () {
+            var oModel = this.getOwnerComponent().getModel(); // Haal OData-model op
+            var oListBinding = oModel.bindList("/Expenses", undefined, undefined, undefined, {
+                $$updateGroupId: "createExpense"
+            });
+
+            // Maak nieuwe context met data uit het model
+            var oContext = oListBinding.create(this.getView().getModel("expenseModel").getData());
+
+            // Verstuur de gegevens naar de backend met batch
+            oModel.submitBatch("createExpense")
                 .then(function () {
-				// De expense is aangemaakt
-				MessageBox.alert("Changes have been saved", {
-					icon : sap.m.MessageBox.Icon.SUCCESS,
-					title : "Success"
-				});
-			}, function (oError) {
-                // Er ging iets fout tijdens het aanmaken van de expense
-				MessageBox.alert(oError.message, {
-					icon : sap.m.MessageBox.Icon.ERROR,
-					title : "Unexpected Error"
-				});
-			});
+                    MessageBox.alert("De gegevens zijn succesvol opgeslagen.", {
+                        icon: sap.m.MessageBox.Icon.SUCCESS,
+                        title: "Succes"
+                    });
+                })
+                .catch(function (oError) {
+                    MessageBox.alert(oError.message, {
+                        icon: sap.m.MessageBox.Icon.ERROR,
+                        title: "Fout"
+                    });
+                });
+        },
+
+        /**
+         * Functie om de huidige expense te verwijderen.
+         */
+        onDelete: function () {
+            MessageBox.confirm("Weet u zeker dat u deze expense wilt verwijderen?", {
+                actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                onClose: function (sAction) {
+                    if (sAction === MessageBox.Action.YES) {
+                        MessageBox.alert("De expense is verwijderd.", {
+                            icon: sap.m.MessageBox.Icon.SUCCESS,
+                            title: "Succes"
+                        });
+                    }
+                }
+            });
         }
     });
 });
